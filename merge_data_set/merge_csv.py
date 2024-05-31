66ac0a94dece7333bd597b782365e12e070fbe14
illTushar <gtushar7668@gmail.com>
2024-05-31 11:34:35 +0530

    last commit

---
+++ merge_data_set/merge_csv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def merge_data_set():
+    read_file1 = pd.read_csv(r'D:\DrugBankCSV\InteractionData1.csv')
+    read_file2 = pd.read_csv(r'D:\DrugBankCSV\InteractionData1.csv')
+    combine_file = [read_file1, read_file2]
+
+    concat_file = pd.concat(combine_file)
+
+    concat_file.reset_index(drop=True, inplace=True)
+
+    concat_file.to_csv(r'C:\Users\gtush\Desktop\csv_folder\Sample_file.csv', index=False)
+    
+
+
+if __name__ == '__main__':
+    merge_data_set()

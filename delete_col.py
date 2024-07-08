8abecc1fea360bfc245de6e47742cbc84620b914
illTushar <gtushar7668@gmail.com>
2024-07-08 10:59:04 +0530

    last commit

--- delete_col.py
+++ delete_col.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     read_csv = pd.read_csv(r'C:\Users\gtush\Desktop\SayaCsv\effect3.csv')
 
     # Drop the 'Unnamed: 0' column and update the DataFrame
-    delete_col = read_csv.drop('Unnamed: 0', axis=1)
+    delete_col = read_csv.drop('Unnamed: ', axis=1)
 
     delete_col.to_csv(r'C:\Users\gtush\Desktop\SayaCsv\effect3.csv', index=False)
 
